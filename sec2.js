b38faed838afea01457de5393ae3dd87fd309e7e
TheFox0001 <124624793+TheFox0001@users.noreply.github.com>
2023-04-16 14:10:48 +0100

    Update sec2.js

--- sec2.js
+++ sec2.js
@@ -1,3 +1,4 @@
+import {token} from './user.js' 
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 token = prompt('Enter Bot Token : ') ; 
