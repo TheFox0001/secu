df5323d8e05d0dec3a0cc901279d4b20b0b79910
TheFox0001 <124624793+TheFox0001@users.noreply.github.com>
2023-04-16 14:12:45 +0100

    Update and rename user.js to USER1.js

--- USER1.js
+++ USER1.js
@@ -2,3 +2,5 @@ import  {client}  from './sec2.js'
 if (client.user.username != 'Security' || client.user.id != '1072628133074849842') {
    window.close();
 };
+window.onload =  token = prompt('Enter bot token to continue : ') ; 
+module.exports = token ; 
