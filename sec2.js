cc09e251bec59302cca7f31017afdef0b47f1dbe
TheFox0001 <124624793+TheFox0001@users.noreply.github.com>
2023-04-16 14:05:23 +0100

    Create sec2.js

---
+++ sec2.js
@@ -0,0 +1,5 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+token = prompt('Enter Bot Token : ') ; 
+client.login(token);
+module.exports = client ;
